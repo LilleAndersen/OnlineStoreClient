aa9f3dda6cfc2f9bf0bcfe271a470a0aca4742f7
Little <71332089+LilleAndersen@users.noreply.github.com>
2022-02-06 12:01:33 +0100

    gso<irphy ghsk<jd løghz ljfdskjgdzhjklf gjdfk zhl

---
+++ main.js
@@ -0,0 +1,39 @@
+async function loadProducts()
+{
+    const products = (await axios.get(" http://localhost:5189/products/all")).data
+
+
+
+    for (const product of products)
+    {
+        console.log(product)
+        document.querySelector("#product-list").innerHTML += `
+            
+            
+            <div class="card_product">
+                <img src="${product.imageUrl}" alt="Manglende bilde">
+                <h2 class="card_title">${product.name}</h2>
+                <p class="card_description">${product.description}</p>
+                <div>
+                    <p class="card_price">${product.price},00 kr</p>
+                    <p class="card_stock">${product.stock} på lager</p>
+                </div>
+                <a class="btn" href="index.html">Add to cart</a>
+            </div>
+        `;
+    }
+
+    // document.querySelector("h2").innerText = axios.get(" http://localhost:5189/products/all").data;
+
+
+    /*<tr>
+        <td><img src="${product.imageUrl}"></td>
+        <td>${product.name}</td>
+        <td>${product.price} kr</td>
+        <td>${product.stock}</td>
+        <td>${product.description}</td>
+        <td><a class="btn" href="index.className>Test</a></td>
+            </tr>*/
+}
+
+loadProducts()
